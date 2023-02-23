cfad43d63c290f9dc966fd17579f1a4c01547975
Neosmart1 <86513591+Aziozo@users.noreply.github.com>
2023-02-23 20:07:14 +0200

    2

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+            let link = document.createElement("link");
+            link.href = "./style/style.css";      /**** your CSS file ****/
+            link.rel = "stylesheet";
+            link.type = "text/css";
+        }
