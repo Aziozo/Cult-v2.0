84871442100f86910f295d2b783af7b96b301856
Neosmart1 <86513591+Aziozo@users.noreply.github.com>
2023-03-09 17:00:29 +0200

    1

--- dist/index.js
+++ dist/index.js
@@ -17,10 +17,10 @@ ScrollSmoother.create({
   });
 
 window.onload = function () {
-            let link = document.createElement("link");
-            link.href = "./style/style.css";      /**** your CSS file ****/
-            link.rel = "stylesheet";
-            link.type = "text/css";
+    let link = document.createElement("link");
+    link.href = "./style/style.css"; 
+    link.rel = "stylesheet";
+    link.type = "text/css";
 }
 
 // ------- payment -----------
