98095014cb309d688eb455f5d1835924382dcc28
Neosmart1 <86513591+Aziozo@users.noreply.github.com>
2023-03-14 11:28:36 +0200

    1

--- dist/main.js
+++ dist/main.js
@@ -7,3 +7,19 @@ else{
     console.log('else');
     $(".current").append(' <h1 class="current-h1">'+art+'</h1>');
 }
+
+
+
+
+checkBox = document.getElementById('checkbox__toggle').addEventListener('click', event => {
+	if(event.target.checked) {
+		$('body').css('overflow-y','hidden');
+		$('html').css('overflow-y','hidden');
+        console.log("hidden");
+	}
+    else{
+        $('body').css('overflow-y','auto');
+		$('html').css('overflow-y','auto');
+        console.log("auto");
+    }
+});
