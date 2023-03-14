9b9499d4799a7e95ae850528d969c81548bb88e5
Neosmart1 <86513591+Aziozo@users.noreply.github.com>
2023-03-14 12:43:17 +0200

    1

--- dist/main.js
+++ dist/main.js
@@ -1,25 +1,37 @@
 art = $('title').text();
-console.log(art);
 if (art == "Культурна Агенція") {
-    
+    art = "Головна"
 }
 else{
     console.log('else');
     $(".current").append(' <h1 class="current-h1">'+art+'</h1>');
 }
 
-
-
-
 checkBox = document.getElementById('checkbox__toggle').addEventListener('click', event => {
 	if(event.target.checked) {
 		$('body').css('overflow-y','hidden');
 		$('html').css('overflow-y','hidden');
-        console.log("hidden");
 	}
     else{
         $('body').css('overflow-y','auto');
-		$('html').css('overflow-y','auto');
-        console.log("auto");
+		$('html').css('overflow-y','auto');    
+    }
+});
+
+// $('.menu-list').find('a').each(function() {
+
+//     if($(this).attr('href')==$(location).attr('href')
+//     ||($(this).attr('href')==$(location).attr('pathname'))){
+//            $(this).parent.addClass("current");
+//            console.log();
+//     }
+// });
+
+$('.menu-list').find('li a').each(function() {
+    
+    if($(this).attr('href')==$(location).attr('href')
+    ||($(this).attr('href')==$(location).attr('pathname'))){
+        
+        $(this).addClass("current");
     }
 });
